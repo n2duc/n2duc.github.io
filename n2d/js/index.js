df452160ba6cbe8286f43040d1cbd4392edfb3bd
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2022-10-07 23:54:47 +0700

    update

--- n2d/js/index.js
+++ n2d/js/index.js
@@ -1,4 +1,4 @@
-const scrollTop = document.querySelector(".scroll-top")
-window.addEventListener("scroll", () => {
-    scrollTop.classList.toggle("scroll-active", window.scrollY > 400)
+const scrollTop = document.querySelector('.scroll-top')
+window.addEventListener('scroll', () => {
+    scrollTop.classList.toggle("scroll-active", window.scrollY > 300)
 })
