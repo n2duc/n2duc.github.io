ad85469f2f600d77479cbdb470bd64ea1905f021
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2023-02-25 16:31:53 +0700

    update

---
+++ js/data.js
@@ -0,0 +1,65 @@
+const dataProject = [
+    {
+        "name": 'NFT',
+        "desc": 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        "image": './images/project-1.png',
+        "color": "#5eae08",
+        "link": "https://n2duc.github.io/landing-page-project/NFT-landing-page/"
+    },
+    {
+        "name": 'Portfolio',
+        "desc": 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        "image": './images/project-2.png',
+        "color": "#525252",
+        "link": "https://portfolion2d.netlify.app/"
+    },
+    {
+        "name": 'Zodiac',
+        "desc": 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        "image": './images/project-3.png',
+        "color": "#f1be5f",
+        "link": "https://12zodiacsigns.netlify.app/"
+    },
+    {
+        "name": 'CD Tet',
+        "desc": 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        "image": './images/project-4.png',
+        "color": "#aa3d3d",
+        "link": "https://demnguoctet2023.netlify.app/"
+    },
+    {
+        "name": 'Portfolio 2',
+        "desc": 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        "image": './images/project-5.png',
+        "color": "#065f46",
+        "link": "https://n2duc.github.io/landing-page-project/Responsive%20Portfolio/"
+    },
+    {
+        "name": 'Halloween',
+        "desc": 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        "image": './images/project-6.png',
+        "color": "#ea580c",
+        "link": "https://n2duc.github.io/landing-page-project/halloween"
+    },
+    {
+        "name": 'Page 1',
+        "desc": 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        "image": './images/project-7.png',
+        "color": "#1d4ed8",
+        "link": "https://n2duc.github.io/landing-page-project/project-landing-page-1/"
+    },
+    {
+        "name": 'Page 2',
+        "desc": 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        "image": './images/project-8.png',
+        "color": "#facc15",
+        "link": "https://n2duc.github.io/landing-page-project/project-landing-page-1/"
+    },
+    {
+        "name": 'TokudaFocus',
+        "desc": 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        "image": './images/project-9.png',
+        "color": "#262626",
+        "link": "https://n2duc.github.io/TokudaFocus/"
+    }
+]
---
+++ js/scirpt.js
@@ -0,0 +1,35 @@
+const modalBtn = document.querySelector('.about-btn')
+const modalOverlay = document.querySelector('.modal-about')
+const closeModalBtn = document.querySelector('.btn-close-modal')
+const listItems = document.querySelector('.content-items')
+
+// Modal
+function openModal() {
+    modalOverlay.classList.remove('hidden')
+}
+function closeModal() {
+    modalOverlay.classList.add('hidden')
+}
+modalBtn.addEventListener('click', openModal)
+closeModalBtn.addEventListener('click', closeModal)
+
+window.onclick = (e) => {
+    if (e.target === modalOverlay) {
+        modalOverlay.classList.add('hidden')
+    }
+}
+
+// Get Data Project
+const Items = [];
+dataProject.map((item) => {
+    Items.push(`<a href="${item.link}" target="_blank" class="item">
+            <h2>${item.name}</h2>
+            <p>${item.desc}</p>
+            <span>Explore it</span>
+            <div class="img">
+                <img src="${item.image}" alt="">
+            </div>
+            <button style="background: ${item.color};"></button>
+        </a>`)
+});
+console.log(listItems.innerHTML = Items.join(""));
